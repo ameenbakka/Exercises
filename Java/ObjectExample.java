1345250ef2ebec9d04b08a49c662301cea3d1ee5
ameen <ameensahil1323@gmail.com>
2024-10-09 10:00:00 +0530

    java

---
+++ Java/ObjectExample.java
@@ -0,0 +1,13 @@
+public class ObjectExample{
+double price=100;
+String color="white";
+int size =30;
+public void display(){
+System.out.println(price);}
+public static void main(String args[])
+{
+ObjectExample shirt = new ObjectExample();
+shirt.display();
+}
+}
+
