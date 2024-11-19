7934561a3b4868a92be6a1e1ce4e145e8bd9ceb0
ameen <ameensahil1323@gmail.com>
2024-11-19 10:00:00 +0530

    java

---
+++ Java/first.java
@@ -0,0 +1,11 @@
+public class First{
+public void display(){
+String name="abc";
+System.out.println(name);
+}
+public static void main(String[] args){
+First obj = new First();
+obj.display();
+}
+}
+
