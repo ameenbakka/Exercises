a8d2999771095b5c2a097140b559ad946bfcd92c
ameen <ameensahil1323@gmail.com>
2025-02-06 10:30:00 +0530

    app2

---
+++ WebApplication2 new/Middleware/Middleware.cs
@@ -0,0 +1,18 @@
+﻿namespace WebApplication2_new.Middleware
+{
+        public class Cm
+        {
+            private readonly RequestDelegate _next;
+            public Cm(RequestDelegate next)
+            {
+                _next = next;
+            }
+            public async Task Invoke(HttpContext context)
+            {
+                Console.WriteLine("Cm - Before Request");
+                await _next(context);
+                Console.WriteLine("Cm After");
+            }
+        }
+
+}
