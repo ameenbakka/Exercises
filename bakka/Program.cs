0b1c57f7199aa8cce8188913ce2c988195196218
ameen <ameensahil1323@gmail.com>
2025-07-01 10:00:00 +0530

    dotnet

---
+++ bakka/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace bakka
+{
+    }
+    class Person
+    {
+        private string name="Ameen";
+
+        public string ameen() // Getter
+        {
+            return name;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Person p = new Person();
+            Console.WriteLine("Name: " + p.ameen());  // Getting value
+        }
+    }
