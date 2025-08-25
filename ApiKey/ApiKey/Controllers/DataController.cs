e81cabf94011d9f0cb4394c3b918803690f4910c
ameen <ameensahil1323@gmail.com>
2025-08-25 10:00:00 +0530

    Apikey

---
+++ ApiKey/ApiKey/Controllers/DataController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiKey.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataController : ControllerBase
+    {
+        [HttpGet]
+        [Authorize(Policy = "ReadOnly")]
+        public IActionResult GetData()
+        {
+            return Ok("GET: Read-only data");
+        }
+
+        [HttpPost]
+        [Authorize(Policy = "WriterOnly")]
+        public IActionResult PostData()
+        {
+            return Ok("POST: Data written");
+        }
+
+        [HttpDelete]
+        [Authorize(Policy = "AdminOnly")]
+        public IActionResult DeleteData()
+        {
+            return Ok("DELETE: Admin only");
+        }
+
+    }
+}
