d93d84575e7214586327bb6fb98b4f5c7aa9c20d
ameen <ameensahil1323@gmail.com>
2025-09-01 10:00:00 +0530

    Apikey

---
+++ ApiKey/ApiKey/Program.cs
@@ -0,0 +1,44 @@
+
+namespace ApiKey
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddAuthorization(options =>
+            {
+                options.AddPolicy("ReadOnly", policy => policy.RequireRole("ReadOnly", "Writer", "Admin"));
+                options.AddPolicy("WriterOnly", policy => policy.RequireRole("Writer", "Admin"));
+                options.AddPolicy("AdminOnly", policy => policy.RequireRole("Admin"));
+            });
+
+
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
